d3c49a44c922cb0449d0d6f6dfe67ea8070ad5da
T1MON <derbindima5@gmail.com>
2023-01-17 13:17:19 +0300

    added server url to docs

--- src/core/helpers/docs.ts
+++ src/core/helpers/docs.ts
@@ -1,12 +1,16 @@
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Env } from '../types/env';
+
+const configService = new ConfigService<Env, true>();
 
 export function setupDocs(app: INestApplication) {
   const config = new DocumentBuilder()
     .setTitle('osu! Pulse')
     .setDescription('The multifunctional music player for osu')
     .setVersion('1.0')
-    .addServer('/api')
+    .addServer(configService.get('URL_API'))
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
