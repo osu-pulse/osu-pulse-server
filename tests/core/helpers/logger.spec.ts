a374664bbcb33ed9b291ff6fda25c5716b11be32
T1MON <derbindima5@gmail.com>
2023-01-18 10:52:16 +0300

    fixed tests

--- tests/core/helpers/logger.spec.ts
+++ tests/core/helpers/logger.spec.ts
@@ -3,17 +3,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { setupLogger } from '../../../src/core/helpers/logger';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { switchAssign } from '../../../src/shared/helpers/switch';
-import { createMock } from '@golevelup/ts-jest';
-import MockedFn = jest.MockedFn;
 
 describe('setupLogger', () => {
   let module: TestingModule;
   let app: INestApplication;
+  let configService: ConfigService;
 
   beforeEach(async () => {
     module = await Test.createTestingModule({
       imports: [ConfigModule],
     }).compile();
+    configService = module.get(ConfigService);
 
     app = module.createNestApplication();
   });
@@ -23,11 +23,35 @@ describe('setupLogger', () => {
     await app.close();
   });
 
-  it('should call useLogger', () => {
+  it('should enable all log levels if debug is enabled', () => {
+    jest.spyOn(configService, 'get').mockImplementation((key) =>
+      switchAssign(key, {
+        DEBUG: true,
+      }),
+    );
     const useLoggerSpy = jest.spyOn(app, 'useLogger');
 
     setupLogger(app);
 
-    expect(useLoggerSpy).toBeCalledTimes(1);
+    expect(useLoggerSpy).toBeCalledWith([
+      'log',
+      'error',
+      'warn',
+      'debug',
+      'verbose',
+    ]);
+  });
+
+  it('should enable only important log levels if debug is disabled', () => {
+    jest.spyOn(configService, 'get').mockImplementation((key) =>
+      switchAssign(key, {
+        DEBUG: false,
+      }),
+    );
+    const useLoggerSpy = jest.spyOn(app, 'useLogger');
+
+    setupLogger(app);
+
+    expect(useLoggerSpy).toBeCalledWith(['log', 'error', 'warn']);
   });
 });
