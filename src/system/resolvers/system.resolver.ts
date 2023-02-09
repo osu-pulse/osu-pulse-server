1c3507a1561925733976e061bca2312c26859ebe
T1MON <derbindima5@gmail.com>
2023-02-09 16:23:34 +0300

    removed health graphql check

--- src/system/resolvers/system.resolver.ts
+++
@@ -1,9 +0,0 @@
-import { Query, Resolver } from '@nestjs/graphql';
-
-@Resolver()
-export class SystemResolver {
-  @Query(() => Boolean)
-  async health(): Promise<boolean> {
-    return true;
-  }
-}
