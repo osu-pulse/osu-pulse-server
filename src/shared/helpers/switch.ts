37faa3b1dd088484d0c7e23748f6ab68ee72e264
T1MON <derbindima5@gmail.com>
2023-02-26 02:48:45 +0300

    made maps for switch partial

--- src/shared/helpers/switch.ts
+++ src/shared/helpers/switch.ts
@@ -3,12 +3,11 @@ export function switchAssign<
   M extends Record<Exclude<V, null | undefined>, unknown>,
 >(
   value: V,
-  map: M,
+  map: Partial<M>,
   fallback?: M[Exclude<typeof value, null | undefined>],
 ): M[Exclude<typeof value, null | undefined>] | undefined {
-  if (value != null) {
-    return map[value as Exclude<V, null | undefined>];
-  }
+  if (value != null) return map[value as Exclude<V, null | undefined>];
+
   return fallback;
 }
 
@@ -17,31 +16,23 @@ export function switchExec<
   M extends Record<Exclude<V, null | undefined>, (value: V) => unknown>,
 >(
   value: V,
-  map: M,
+  map: Partial<M>,
   fallback?: M[Exclude<typeof value, null | undefined>],
 ): ReturnType<M[Exclude<typeof value, null | undefined>]> | undefined {
   if (value != null) {
     const handler = map[value as Exclude<V, null | undefined>];
-    if (handler) {
-      return handler(value) as any;
-    }
+    if (handler) return handler(value) as any;
   }
-  if (fallback) {
-    return fallback(value) as any;
-  }
+  if (fallback) return fallback(value) as any;
 }
 
 export function switchThrow<
   V extends string | number | null | undefined,
   M extends Record<Exclude<V, null | undefined>, unknown>,
->(value: V, map: M, fallback?: unknown): never | void {
+>(value: V, map: Partial<M>, fallback?: unknown): never | void {
   if (value != null) {
     const reason = map[value as Exclude<V, null | undefined>];
-    if (reason) {
-      throw reason;
-    }
+    if (reason) throw reason as unknown as Error;
   }
-  if (fallback) {
-    throw fallback;
-  }
+  if (fallback) throw fallback as unknown as Error;
 }
